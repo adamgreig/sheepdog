64c3d5d99820078f2131d4417570085cfb2204a0
Adam Greig <adam@adamgreig.com>
2013-12-03 15:40:04 +0000

    Add Client code and tests

---
+++ sheepdog/sheep/client.py
@@ -0,0 +1,69 @@
+"""
+Sheepdog
+Copyright 2013 Adam Greig
+
+Clientside code.
+"""
+
+import json
+import types
+import base64
+import marshal
+import requests
+
+class Client:
+    """Find out what to do, do it, report back."""
+    HTTP_RETRIES = 10
+
+    def __init__(self, url, request_id, job_index):
+        self.url = url
+        self.request_id = request_id
+        self.job_index = job_index
+
+    def get_details(self):
+        """Retrieve the function to run and arguments to run with from the
+           server.
+        """
+        url = self.url + "?request_id={0}&job_index={1}"
+        url = url.format(self.request_id, self.job_index)
+        tries = 0
+        while tries < self.HTTP_RETRIES:
+            try:
+                r = requests.get(url)
+                break
+            except (requests.ConnectionError, requests.Timeout):
+                tries += 1
+                continue
+        if tries == self.HTTP_RETRIES:
+            raise RuntimeError("Could not connect to server.")
+       
+        r = r.json()
+        func_bin = base64.b64decode(r['func'])
+        args_bin = base64.b64decode(r['args'])
+        code = marshal.loads(func_bin)
+        self.func = types.FunctionType(code, globals(), "f")
+        self.args = marshal.loads(args_bin)
+
+    def run(self):
+        """Run the downloaded function, storing the result."""
+        if not hasattr(self, 'func') or not hasattr(self, 'args'):
+            raise RuntimeError("Must call `get_details` before `run`.")
+        self.result = self.func(*self.args)
+
+    def submit_results(self):
+        if not hasattr(self, 'result'):
+            raise RuntimeError("Must call `run` before `submit_results`.")
+        result = base64.b64encode(marshal.dumps(self.result))
+        tries = 0
+        while tries < self.HTTP_RETRIES:
+            try:
+                r = requests.post(self.url,
+                                  data=dict(request_id=self.request_id,
+                                            job_index=self.job_index,
+                                            result = result))
+                break
+            except (requests.ConnectionError, requests.Timeout):
+                tries += 1
+                continue
+        if tries == self.HTTP_RETRIES:
+            raise RuntimeError("Could not submit results to server.")
---
+++ sheepdog/sheep/tests/test_client.py
@@ -0,0 +1,78 @@
+"""
+Sheepdog
+Copyright 2013 Adam Greig
+
+Client test code.
+"""
+
+import os
+import socket
+import marshal
+import tempfile
+from nose.tools import assert_equal, assert_raises
+
+from sheepdog.dog import storage, server
+from sheepdog.sheep import client
+
+def get_free_port():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", 0))
+    port = s.getsockname()[1]
+    s.close()
+    return port
+
+def my_function(a, b):
+    return a + b
+
+class TestClient:
+    def setup(self):
+        self.db_fd, self.dbfile = tempfile.mkstemp()
+        self.storage = storage.Storage(dbfile=self.dbfile)
+        self.storage.initdb()
+
+        self.func = my_function
+        self.args = (3, 4)
+        self.request_id = 1
+        self.job_index = 2
+
+        self.func_bin = marshal.dumps(my_function.__code__)
+        self.args_bin = [marshal.dumps((1, 1)), marshal.dumps(self.args)]
+
+        self.storage.new_request(self.func_bin, self.args_bin)
+
+        self.port = get_free_port()
+        self.server = server.Server(port=self.port, dbfile=self.dbfile)
+
+        self.url = "http://localhost:{0}/".format(self.port)
+        self.client = client.Client(self.url, self.request_id, self.job_index)
+
+    def teardown(self):
+        self.server.stop()
+        os.close(self.db_fd)
+        os.unlink(self.dbfile)
+
+    def test_gets_details(self):
+        self.client.get_details()
+        assert self.client.args == (3, 4)
+        assert type(self.client.func) == type(my_function)
+
+    def test_doesnt_run_without_details(self):
+        assert_raises(RuntimeError, self.client.run)
+
+    def test_runs_function(self):
+        self.client.get_details()
+        self.client.run()
+        assert hasattr(self.client, 'result')
+        assert self.client.result == self.func(*self.args)
+
+    def test_doesnt_submit_without_running(self):
+        assert_raises(RuntimeError, self.client.submit_results)
+        self.client.get_details()
+        assert_raises(RuntimeError, self.client.submit_results)
+
+    def test_submits_results(self):
+        self.client.get_details()
+        self.client.run()
+        self.client.submit_results()
+        assert_equal(self.storage.get_results(self.request_id),
+            [(self.args_bin[1], marshal.dumps(self.func(*self.args)))])
