ba7b9b96264a4db3edf78cd52c42f68adccfba27
Adam Greig <adam@adamgreig.com>
2013-12-03 19:00:10 +0000

    fix incorrect scp/ssh port spec

--- sheepdog/dog/deployment.py
+++ sheepdog/dog/deployment.py
@@ -19,14 +19,16 @@ def deploy_and_run(host, jobfile, request_id, user=None, port=22):
     if user is None:
         user = os.getlogin()
 
-    hoststr = "{0}@{1}:{2}".format(user, host, port)
+    hoststr = "{0}@{1}".format(user, host)
+    scpportstr = "-P {0}".format(port)
+    sshportstr = "-p {0}".format(port)
     filestr = "{0}:~/sheepdog_{1}.py".format(hoststr, request_id)
     cmdstr = "qsub ~/sheepdog_{0}.py".format(request_id)
 
     fd, fp = tempfile.mkstemp()
     os.write(fd, jobfile.encode())
 
-    rv = subprocess.call(["scp", fp, filestr])
+    rv = subprocess.call(["scp", scpportstr, fp, filestr])
 
     os.close(fd)
     os.unlink(fp)
@@ -34,7 +36,7 @@ def deploy_and_run(host, jobfile, request_id, user=None, port=22):
     if rv:
         raise RuntimeError("Error uploading job file.")
 
-    rv = subprocess.call(["ssh", hoststr, cmdstr])
+    rv = subprocess.call(["ssh", sshportstr, hoststr, cmdstr])
 
     if rv:
         raise RuntimeError("Error submitting job request.")
