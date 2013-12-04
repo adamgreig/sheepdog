48b269809936e8e61a09f9e5dd8360dc578a6c7a
Adam Greig <adam@adamgreig.com>
2013-12-04 16:34:56 +0000

    packaging stuff. oh and swap back to markdown readme.

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -4,6 +4,8 @@ Changelog
 Version 0.1
 -----------
 
+0.1.0
+^^^^^
 Released on 2013-12-04. First release.
 
 Contains :py:func:`sheepdog.map_sync`, the first top level
---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ''
+
+setup(
+    name='Sheepdog',
+    version='0.1.0',
+    author='Adam Greig',
+    author_email='adam@adamgreig.com',
+    packages=['sheepdog', 'sheepdog.dog', 'sheepdog.sheep'],
+    url='http://sheepdog.readthedocs.org',
+    license='MIT',
+    description='Shepherd GridEngine',
+    long_description=long_description,
+    install_requires=[
+        "Flask >= 0.10.1",
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Scientific/Engineering',
+    ],
+)
